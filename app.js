f7f88e89cc86ea4598735b65c8c0c4a7c1da1326
S.Zelensky <zelenstein@gmail.com>
2024-01-31 17:07:00 +0300

    v2.1

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-document
-  .getElementById('drop_zone')
-  .addEventListener('drop', handleFileSelect, false);
+// Получаем элементы
+const dropZone = document.getElementById('drop_zone');
+const dropZoneWindow = document.querySelector('.drop_zone_window');
+const fileInput = document.getElementById('fileInput');
+const resultList = document.getElementById('resultList');
 
+// Обработчик перетаскивания файла на dropZone
+function handleDragOver(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'copy';
+}
+
+// Обработчик выбора файла
 function handleFileSelect(event) {
   event.stopPropagation();
   event.preventDefault();
 
-  const files = event.dataTransfer.files;
-
-  sendFileToServer(files[0]);
+  const files = event.dataTransfer
+    ? event.dataTransfer.files
+    : event.target.files;
+  if (files.length > 0) {
+    sendFileToServer(files[0]);
+  }
 }
 
+// Отправка файла на сервер
 function sendFileToServer(file) {
   const formData = new FormData();
   formData.append('file', file);
@@ -21,38 +35,43 @@ function sendFileToServer(file) {
   })
     .then(response => response.text())
     .then(data => {
-      const resultList = document.getElementById('resultList');
-      resultList.innerHTML = '';
-
-      const results = data
-        .split(',')
-        .map(result => result.trim().replace(/\n/g, ''));
-      console.log('results:', results);
-
-      if (results.length === 1 && results[0] === '') {
-        const listItem = document.createElement('li');
-        listItem.textContent = 'Сканирование завершено, проблем не найдено';
-        resultList.appendChild(listItem);
-      } else {
-        results.forEach(result => {
-          if (result !== '') {
-            const listItem = document.createElement('li');
-            listItem.textContent = result;
-            resultList.appendChild(listItem);
-          }
-        });
-      }
+      updateResultList(data);
     })
     .catch(error => {
       console.error('Ошибка:', error);
     });
 }
 
-function handleDragOver(event) {
-  event.stopPropagation();
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'copy';
+// Обновление списка результатов
+function updateResultList(data) {
+  resultList.innerHTML = '';
+  const results = data
+    .split(',')
+    .map(result => result.trim().replace(/\n/g, ''));
+
+  if (results.length === 1 && results[0] === '') {
+    appendToList('Сканирование завершено, проблем не найдено');
+  } else {
+    results.forEach(result => {
+      if (result !== '') {
+        appendToList(result);
+      }
+    });
+  }
 }
 
-const dropZone = document.getElementById('drop_zone');
+// Добавление элемента в список
+function appendToList(text) {
+  const listItem = document.createElement('li');
+  listItem.textContent = text;
+  resultList.appendChild(listItem);
+}
+
+// Назначение обработчиков событий
 dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
+dropZoneWindow.addEventListener('click', () => {
+  fileInput.click();
+});
+
+fileInput.addEventListener('change', handleFileSelect);
