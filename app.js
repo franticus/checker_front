0d04136b526fcd2013ca5545b44e44cf3861e3a9
S.Zelensky <zelenstein@gmail.com>
2024-01-31 10:45:53 +0300

    v1.3

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ function handleFileSelect(event) {
   event.stopPropagation();
   event.preventDefault();
 
-  var files = event.dataTransfer.files;
+  const files = event.dataTransfer.files;
 
   sendFileToServer(files[0]);
 }
 
 function sendFileToServer(file) {
-  var formData = new FormData();
+  const formData = new FormData();
   formData.append('file', file);
 
   fetch('http://localhost:3000/upload', {
@@ -21,22 +21,22 @@ function sendFileToServer(file) {
   })
     .then(response => response.text())
     .then(data => {
-      var resultList = document.getElementById('resultList');
+      const resultList = document.getElementById('resultList');
       resultList.innerHTML = '';
 
-      var results = data
+      const results = data
         .split(',')
         .map(result => result.trim().replace(/\n/g, ''));
       console.log('results:', results);
 
-      if (results.length === 1) {
-        var listItem = document.createElement('li');
+      if (results.length === 1 && results[0] === '') {
+        const listItem = document.createElement('li');
         listItem.textContent = 'Сканирование завершено, проблем не найдено';
         resultList.appendChild(listItem);
       } else {
         results.forEach(result => {
           if (result !== '') {
-            var listItem = document.createElement('li');
+            const listItem = document.createElement('li');
             listItem.textContent = result;
             resultList.appendChild(listItem);
           }
@@ -54,5 +54,5 @@ function handleDragOver(event) {
   event.dataTransfer.dropEffect = 'copy';
 }
 
-var dropZone = document.getElementById('drop_zone');
+const dropZone = document.getElementById('drop_zone');
 dropZone.addEventListener('dragover', handleDragOver, false);
