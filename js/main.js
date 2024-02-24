80cf034f79983ef207a8e24b19024df8a3b70d66
S.Zelensky <zelenstein@gmail.com>
2024-02-24 14:48:25 +0300

    bgcolor

--- js/main.js
+++ js/main.js
@@ -19,32 +19,30 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     document.cookie = name + '=' + (value || '') + expires + '; path=/';
   }
+});
 
-  // Функция для получения куки
-  function getCookie(name) {
-    let nameEQ = name + '=';
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+// Функция для получения куки
+function getCookie(name) {
+  let nameEQ = name + '=';
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
+  return null;
+}
 
-  // Проверяем, сохранена ли тема в куки, и применяем её
-  let savedTheme = getCookie('theme');
-  if (savedTheme === 'dark') {
-    document.body.classList.add('dark-mode');
-    themeSwitcher.checked = true;
-  }
+// Проверяем, сохранена ли тема в куки, и применяем её
+let savedTheme = getCookie('theme');
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+  themeSwitcher.checked = true;
+}
 
-  // Слушатель события для переключателя
-  themeSwitcher.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
-    let theme = document.body.classList.contains('dark-mode')
-      ? 'dark'
-      : 'light';
-    setCookie('theme', theme, 7); // Сохраняем выбранную тему на 7 дней
-  });
+// Слушатель события для переключателя
+themeSwitcher.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
+  let theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+  setCookie('theme', theme, 7); // Сохраняем выбранную тему на 7 дней
 });
