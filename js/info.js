cbfd1be529435acbdedc0335d200d1f28be2573f
S.Zelensky <zelenstein@gmail.com>
2024-02-27 10:58:24 +0300

    resetUploadsButton

---
+++ js/info.js
@@ -0,0 +1,27 @@
+const resetUploadsButton = document.getElementById('resetUploadsButton');
+
+const url = 'https://checker-zip-frantunn.amvera.io';
+// const url = 'http://localhost:3000';
+
+resetUploadsButton.addEventListener('click', function () {
+  fetch('/cleanuploads', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error('Something went wrong on server');
+    })
+    .then(data => {
+      console.log(data);
+      console.log('Uploads directory has been cleared successfully.');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      console.log('Error clearing uploads directory');
+    });
+});
