3392592ff0615a0261b2d1595fd5d861eaf92414
S.Zelensky <zelenstein@gmail.com>
2024-02-27 11:39:24 +0300

    resetUploadsButton

--- js/info.js
+++ js/info.js
@@ -3,23 +3,28 @@ const resetUploadsButton = document.getElementById('resetUploadsButton');
 const url = 'https://checker-zip-frantunn.amvera.io';
 // const url = 'http://localhost:3000';
 
-resetUploadsButton.addEventListener('click', function () {
-  fetch(`${url}/cleanuploads`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      console.log('response:', response);
-      if (response.ok) {
-        return response.text();
-      }
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
+resetUploadsButton.addEventListener('click', async function () {
+  try {
+    const response = await fetch(`${url}/cleanuploads`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
+
+    console.log('response:', response);
+
+    if (response.ok) {
+      const data = await response.text();
+      console.log(data);
+    } else {
+      console.error(
+        'Server responded with an error:',
+        response.status,
+        response.statusText
+      );
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
